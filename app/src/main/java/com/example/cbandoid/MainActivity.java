a6c80bc3c82e94d3e37db9a7d38807b2615c671e
Cody Peterson <CGPeterson@student.fullsail.edu>
2020-10-30 13:26:21 -0600

    Added sub menu and implemented

--- app/src/main/java/com/example/cbandoid/MainActivity.java
+++ app/src/main/java/com/example/cbandoid/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.cbandoid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button DiceRoller;
+    ImageView Submenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_menu);
 
         DiceRoller=findViewById(R.id.DiceRollButton);
+        Submenu=findViewById(R.id.mainpopupbutton);
 
         DiceRoller.setOnClickListener(v ->{
             RollerActivity roller = new RollerActivity();
             startActivity(new Intent(MainActivity.this, RollerActivity.class));
+        });
+
+        Submenu.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, SubmenuActivity.class));
         });
     }
 
