39852bf84091b758daf6ec4b0ddd9593948cef1a
Cody Peterson <CGPeterson@student.fullsail.edu>
2020-10-30 13:01:50 -0600

    Added functionallity to Menu Roller button

--- app/src/main/java/com/example/cbandoid/MainActivity.java
+++ app/src/main/java/com/example/cbandoid/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.cbandoid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button DiceRoller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
 
+        DiceRoller=findViewById(R.id.DiceRollButton);
+
+        DiceRoller.setOnClickListener(v ->{
+            RollerActivity roller = new RollerActivity();
+            startActivity(new Intent(MainActivity.this, RollerActivity.class));
+        });
     }
 
 }
