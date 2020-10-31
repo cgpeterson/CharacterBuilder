cf57df18139e5138fe1dcc4310c94464d15d5c77
Cody Peterson <CGPeterson@student.fullsail.edu>
2020-10-31 09:42:35 -0600

    fixed sub menu to be popup menu

--- app/src/main/java/com/example/cbandoid/MainActivity.java
+++ app/src/main/java/com/example/cbandoid/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.cbandoid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button DiceRoller;
-    ImageView Submenu;
+    ImageView pmenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +22,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_menu);
 
         DiceRoller=findViewById(R.id.DiceRollButton);
-        Submenu=findViewById(R.id.mainpopupbutton);
+        pmenu=findViewById(R.id.mainpopupbutton);
 
         DiceRoller.setOnClickListener(v ->{
-            RollerActivity roller = new RollerActivity();
             startActivity(new Intent(MainActivity.this, RollerActivity.class));
         });
 
-        Submenu.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, SubmenuActivity.class));
+        pmenu.setOnClickListener(this::showMenu);
+    }
+
+    private void showMenu(View v) {
+        PopupMenu popMenu = new PopupMenu(MainActivity.this, v);
+        popMenu.getMenuInflater().inflate(R.menu.popup_menu,popMenu.getMenu());
+        popMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.popup_profile) {
+                    //Placeholder code
+                    Toast.makeText(MainActivity.this, "You clicked Profile", Toast.LENGTH_SHORT).show();
+                }
+                if(item.getItemId() == R.id.popup_help) {
+                    //Placeholder code
+                    Toast.makeText(MainActivity.this, "You clicked Help", Toast.LENGTH_SHORT).show();
+                }
+                if(item.getItemId() == R.id.popup_settings) {
+                    //Placeholder code
+                    Toast.makeText(MainActivity.this, "You clicked Settings", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
         });
+        popMenu.show();
     }
 
 }
