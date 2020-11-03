d2eb82fbd8fb4585f7adaf916b78b64534b46433
reddylant <DWThomas@student.fullsail.edu>
2020-11-03 16:31:21 -0600

    Help Screen and HelpActivity(incomplete)

---
+++ app/src/main/java/com/example/cbandoid/HelpActivity.java
@@ -0,0 +1,13 @@
+package com.example.cbandoid;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpActivity extends AppCompatActivity
+{
+    TextView codydis;
+    TextView dylandis;
+    @Override
+}
