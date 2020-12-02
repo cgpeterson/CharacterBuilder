25d9efa9813cb5cd79e01d0ef515c22eb6a66cc6
Cody Peterson <CGPeterson@student.fullsail.edu>
2020-12-01 18:36:59 -0700

    cleaned up warnings in ExpertCreateChar

--- app/src/main/java/com/example/cbandoid/ExpertCreateChar.java
+++ app/src/main/java/com/example/cbandoid/ExpertCreateChar.java
@@ -1,12 +1,10 @@
 package com.example.cbandoid;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
